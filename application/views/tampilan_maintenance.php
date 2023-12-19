7a48313230ee4639be407b3e2faecd60095aa5ee
Rahmadika S Setiawan <dikakewren@outlook.com>
2023-12-19 22:21:42 +0700

    MVC MAINTENANCE UPDATE

---
+++ application/views/tampilan_maintenance.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Bursa Kerja Khusus - SMK Tunas Harapan Jakarta Barat</title>
+    <meta content="Bursa Kerja Khusus - SMK Tunas Harapan Jakarta Barat" name="description">
+    <meta content="BKK SMK TH" name="keywords">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="<?= base_url() ?>assets/img/bkk_terbaru.png" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/maintenance/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/maintenance/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/maintenance/fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/maintenance/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/maintenance/vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/maintenance/css/util.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/maintenance/css/main.css">
+    <!--===============================================================================================-->
+</head>
+
+<body>
+
+
+    <div class="bg-img1 overlay1 size1 flex-w flex-c-m p-t-55 p-b-55 p-l-15 p-r-15" style="background-image: url('https://smkth-jakbar.com/assets/images/background3.jpeg');">
+        <div class="wsize1">
+            <p class="txt-center p-b-23">
+                <img src="<?= base_url() ?>assets/img/bkk_terbaru.png" alt="logo_bkk" style="width: 200px;height 200px">
+            </p>
+
+            <h3 class="l1-txt1 txt-center p-b-22">
+                Coming Soon
+            </h3>
+
+            <h3 class="l1-txt1 txt-center p-b-22">
+                website bkk sedang dalam perbaikan
+            </h3>
+        </div>
+    </div>
+
+
+
+
+
+    <!--===============================================================================================-->
+    <script src="<?= base_url() ?>assets/maintenance/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url() ?>assets/maintenance/vendor/bootstrap/js/popper.js"></script>
+    <script src="<?= base_url() ?>assets/maintenance/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url() ?>assets/maintenance/vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+
+    <script src="<?= base_url() ?>assets/maintenance/js/main.js"></script>
+
+</body>
+
+</html>
